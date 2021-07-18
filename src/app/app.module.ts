17d04a9c46eb93df03cf5ddcb4d6785f7c987689
BrianKhaifah <briankhaifah@gmail.com>
2021-07-19 01:30:44 +0300

    modified the background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    QuotesComponent
+    QuotesComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
